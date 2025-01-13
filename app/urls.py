ee11a4c6c1c3c89e274274f9780bafb55effc43c
Felifelps <felipefelipe23456@gmail.com>
2025-01-13 15:15:29 -0300

    Activate db update

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('activate_db/', views.activate_db, name='activate_db'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from categories.models import Category
 
 
 def home(request):
     url = 'list-loans' if request.user.is_authenticated else 'login'
     return HttpResponseRedirect(reverse(url))
+
+
+def activate_db(request):
+    count = Category.objects.count()
+    return HttpResponse(
+        content=f"Activated succesfully ({count})!"
+    )
